40aba9e27bc773f2cfd578c2eb134074f488d0f7
laughing man <komarovandrey396@gmail.com>
2022-01-13 22:52:04 +0300

    full refactoring

--- main.py
+++ main.py
@@ -1,78 +1,67 @@
 '''Вариант  222222'''
 
-a = ['*', '-', '/', '=', '>', '<', '>=', '=', '<=', '+','<>']
-b = ['(', ')', '"', ';', '!']
-c = ['long','float','static',
-   'char','short','switch','int',
-   'const','if','then','else','for'
-   ,'while','break']
+import sys
+import re
 
-def is_letter(Char):
-    if ((Char >= 'a' and Char <= 'z') or (Char >= 'A' and Char <= 'Z')):
-        return True
-    else:
-        return False
+RESERVED = 'RESERVED'
+INT      = 'INT'
+ID       = 'ID'
+
+token_exprs = [
+    (r'[ \n\t]+',              None),
+    (r'#[^\n]*',               None),
+    (r'\:=',                   RESERVED),
+    (r'\(',                    RESERVED),
+    (r'\)',                    RESERVED),
+    (r';',                     RESERVED),
+    (r'\+',                    RESERVED),
+    (r'-',                     RESERVED),
+    (r'\*',                    RESERVED),
+    (r'/',                     RESERVED),
+    (r'<=',                    RESERVED),
+    (r'<',                     RESERVED),
+    (r'>=',                    RESERVED),
+    (r'>',                     RESERVED),
+    (r'=',                     RESERVED),
+    (r'!=',                    RESERVED),
+    (r'and',                   RESERVED),
+    (r'or',                    RESERVED),
+    (r'not',                   RESERVED),
+    (r'if',                    RESERVED),
+    (r'then',                  RESERVED),
+    (r'else',                  RESERVED),
+    (r'while',                 RESERVED),
+    (r'do',                    RESERVED),
+    (r'end',                   RESERVED),
+    (r'[0-9]+',                INT),
+    (r'[A-Za-z][A-Za-z0-9_]*', ID),
+]
+
+def lex(characters, token_exprs):
+	pos = 0
+	tokens = []
+	while pos < len(characters):
+		match = None
+		for token_expr in token_exprs:
+			pattern, tag = token_expr
+			regex = re.compile(pattern)
+			match = regex.match(characters, pos)
+			if match:
+				text = match.group(0)
+				if tag:
+					token = (text, tag)
+					tokens.append(token)
+				break
+		if not match:
+			sys.stderr.write('Illegal character: %s\n' % characters[pos])
+			sys.exit(1)
+		else:
+			pos = match.end(0)
+	return tokens
 
 
-def is_digit(Char):
-    if (Char <= '9' and Char >= '0'):
-        return True
-    else:
-        return False
-
-
-def is_space(Char):
-    if (Char == ' '):
-        return True
-    else:
-        return False
-
-
-def lexer(List):
-    ResultList = []
-    for String in List:
-        Letter = ''
-        letter = ''
-        index = 0
-        for Char in String:
-            if (index < len(String) - 1):
-                index += 1
-            if (is_letter(Char) or is_digit(Char)):
-                if (is_letter(String[index]) or is_digit(String[index])):
-                    Letter += Char
-                elif (is_space(String[index]) or (String[index] in b) or (
-                        String[index] in a) or (String[index:index + 2] in a)):
-                    Letter += Char
-                    ResultList.append(Letter)
-                    Letter = ''
-            elif Char in b:
-                        ResultList.append(Char)
-            elif Char in a:
-                        letter += Char
-                        if (String[index] in a):
-                            letter += String[index]
-                            ResultList.append(letter)
-                            letter = ''
-                        else:
-                            ResultList.append(letter)
-                            letter = ''
-            elif is_space(Char):
-                            pass
-    return ResultList
-
-def pretty_printer(char):
-    for i in char:
-        if i in a:
-            print("Оператор"+i+">")
-        elif i in b:
-            print("<Defense",+i+">")
-        elif i in c:
-            print("<Ключевое слово", +i+">")
-        elif i.isdigit():
-            print("Постоянный,"+i+">")
-        elif i.isalnum():
-            print("<Идентификатор," + i + ">")
-
+def imp_lex(characters):
+    return lex(characters, token_exprs)
 
 if __name__ == "__main__":
     p=[]
