0dbf50d5af45ade76090784b79688659f0746ce9
laughing man <komarovandrey396@gmail.com>
2022-01-13 22:32:36 +0300

    added pretty printing function

--- main.py
+++ main.py
@@ -7,28 +7,28 @@ c = ['long','float','static',
    'const','if','then','else','for'
    ,'while','break']
 
-def isLetter(Char):
+def is_letter(Char):
     if ((Char >= 'a' and Char <= 'z') or (Char >= 'A' and Char <= 'Z')):
         return True
     else:
         return False
 
 
-def isDigit(Char):
+def is_digit(Char):
     if (Char <= '9' and Char >= '0'):
         return True
     else:
         return False
 
 
-def IsSpace(Char):
+def is_space(Char):
     if (Char == ' '):
         return True
     else:
         return False
 
 
-def fenli(List):
+def lexer(List):
     ResultList = []
     for String in List:
         Letter = ''
@@ -37,10 +37,10 @@ def fenli(List):
         for Char in String:
             if (index < len(String) - 1):
                 index += 1
-            if (IsLetter(Char) or IsDigit(Char)):
-                if (IsLetter(String[index]) or IsDigit(String[index])):
+            if (is_letter(Char) or is_digit(Char)):
+                if (is_letter(String[index]) or is_digit(String[index])):
                     Letter += Char
-                elif (IsSpace(String[index]) or (String[index] in b) or (
+                elif (is_space(String[index]) or (String[index] in b) or (
                         String[index] in a) or (String[index:index + 2] in a)):
                     Letter += Char
                     ResultList.append(Letter)
@@ -56,9 +56,30 @@ def fenli(List):
                         else:
                             ResultList.append(letter)
                             letter = ''
-            elif IsSpace(Char):
+            elif is_space(Char):
                             pass
     return ResultList
 
+def pretty_printer(char):
+    for i in char:
+        if i in a:
+            print("Оператор"+i+">")
+        elif i in b:
+            print("<Defense",+i+">")
+        elif i in c:
+            print("<Ключевое слово", +i+">")
+        elif i.isdigit():
+            print("Постоянный,"+i+">")
+        elif i.isalnum():
+            print("<Идентификатор," + i + ">")
+
+
 if __name__ == "__main__":
-    pass
+    p=[]
+    with open("token_file.txt",'r') as f:
+        m=f.readlines()
+        for i in m:
+            n=i.strip()
+            p.append(n)
+        y=lexer(p)
+        pretty_printer(y)
